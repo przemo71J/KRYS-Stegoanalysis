85ca74b85317a90663a4043a01f37c9d049f1ad3
Przemyslaw Pustelnik <przemyslaw.pustelnik@nokia.com>
2025-01-25 17:28:53 +0100

    paths fixed

--- histogram/1_channel.py
+++ histogram/1_channel.py
@@ -1,42 +1,49 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 def calculate_histogram(image):
     histogram, bins = np.histogram(image.flatten(), bins=256, range=[0, 256])
     return histogram
 
-def plot_histogram(histogram, title):
+def plot_histogram(histogram, title, filename):
+    output_dir = os.path.dirname(filename)
+    os.makedirs(output_dir, exist_ok=True)
+
     plt.figure(figsize=(8, 6))
     plt.bar(range(256), histogram, color='red')
     plt.title(title)
     plt.xlabel('Intensywność pikseli')
     plt.ylabel('Liczba wystąpień')
-    plt.show()
 
-def detect_stego_changes(original_image, stego_image):
+    plt.savefig(filename)
+    plt.close()
 
-    # Oblicz histogramy
+def detect_stego_changes(original_image, stego_image, method, img_name):
+    save_dir = f"histogram_images/{img_name}/{method}"
+    os.makedirs(save_dir, exist_ok=True)
+
     original_histogram = calculate_histogram(original_image)
     stego_histogram = calculate_histogram(stego_image)
 
-    # Porównaj histogramy
     diff = np.abs(original_histogram - stego_histogram)
 
-    # Rysowanie wyników
-    plot_histogram(original_histogram, 'Histogram obrazu oryginalnego')
-    plot_histogram(stego_histogram, 'Histogram obrazu z ukrytymi danymi')
-    plot_histogram(diff, 'Różnica między histogramami')
+    original_hist_filename = f"{save_dir}/{img_name}_original_histogram.png"
+    stego_hist_filename = f"{save_dir}/{img_name}_stego_histogram.png"
+    diff_hist_filename = f"{save_dir}/{img_name}_difference_histogram.png"
 
-    # Wyświetlenie metryk różnicowych
-    print("Suma różnic między histogramami:", np.sum(diff))
+    plot_histogram(original_histogram, 'Histogram obrazu oryginalnego - analiza Grayscale', original_hist_filename)
+    plot_histogram(stego_histogram, 'Histogram obrazu z ukrytymi danymi - analiza Grayscale', stego_hist_filename)
+    plot_histogram(diff, 'Różnica między histogramami - analiza Grayscale', diff_hist_filename)
 
-img = 'neon.png'
-path = 'C:/Users/prpustel/Desktop/PROJEKT_KRYS/KRYS-Stegoanalysis/'
-method = 'dct'
-original_image = cv2.imread(path + 'images/'+img, cv2.IMREAD_GRAYSCALE)
-stego_filename = path + method+"_images/"+ img.split('.')[0] +'.'+ img.split('.')[1]
-stego_image = cv2.imread(stego_filename, cv2.IMREAD_GRAYSCALE)
+image_dir = './images/'
+methods = ['lsb', 'rgba', 'dct']
 
-# Wykrywanie zmian
-detect_stego_changes(original_image, stego_image)
+for img in os.listdir(image_dir):
+    if img.endswith(('.png', '.jpg', '.jpeg')):
+        for method in methods:
+            original_image = cv2.imread(os.path.join(image_dir, img), cv2.IMREAD_GRAYSCALE)
+            stego_filename = os.path.join(f'./{method}_images/', img)
+            stego_image = cv2.imread(stego_filename, cv2.IMREAD_GRAYSCALE)
+            detect_stego_changes(original_image, stego_image, method, img.split('.')[0])
--- histogram/3_channel.py
+++ histogram/3_channel.py
@@ -1,50 +1,59 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 def calculate_histogram(image):
     histogram, bins = np.histogram(image.flatten(), bins=256, range=[0, 256])
     return histogram
 
-def plot_histogram(histogram, title, color):
+def plot_histogram(histogram, title, color, save_path=None):
     plt.figure(figsize=(8, 6))
     plt.bar(range(256), histogram, color=color)
     plt.title(title)
     plt.xlabel('Intensywność pikseli')
     plt.ylabel('Liczba wystąpień')
-    plt.show()
 
-def detect_stego_changes_rgb(original_image, stego_image):
+    if save_path:
+        plt.savefig(save_path)
+    else:
+        plt.show()
+
+    plt.close()
+
+def detect_stego_changes_rgb(original_image, stego_image, method, img_name):
+    save_dir = f"histogram_images_rgb/{img_name}/{method}"
+    os.makedirs(save_dir, exist_ok=True)
+    
     channels = ['Blue', 'Green', 'Red']
     colors = ['blue', 'green', 'red']
     
     for i, channel in enumerate(channels):
-        # Wyciągnięcie pojedynczego kanału
         original_channel = original_image[:, :, i]
         stego_channel = stego_image[:, :, i]
 
-        # Obliczanie histogramów dla danego kanału
         original_histogram = calculate_histogram(original_channel)
         stego_histogram = calculate_histogram(stego_channel)
 
-        # Porównanie histogramów
         diff = np.abs(original_histogram - stego_histogram)
 
-        # Rysowanie wyników
-        plot_histogram(original_histogram, f'Histogram obrazu oryginalnego ({channel})', colors[i])
-        plot_histogram(stego_histogram, f'Histogram obrazu z ukrytymi danymi ({channel})', colors[i])
-        plot_histogram(diff, f'Różnica między histogramami ({channel})', colors[i])
+        original_hist_filename = f"{save_dir}/{img_name}_original_histogram_{channel}.png"
+        stego_hist_filename = f"{save_dir}/{img_name}_stego_histogram_{channel}.png"
+        diff_hist_filename = f"{save_dir}/{img_name}_difference_histogram_{channel}.png"
 
-        # Wyświetlenie metryk różnicowych
+        plot_histogram(original_histogram, f'Histogram obrazu oryginalnego ({channel})', colors[i], original_hist_filename)
+        plot_histogram(stego_histogram, f'Histogram obrazu z ukrytymi danymi ({channel})', colors[i], stego_hist_filename)
+        plot_histogram(diff, f'Różnica między histogramami ({channel})', colors[i], diff_hist_filename)
+
         print(f"Suma różnic między histogramami dla kanału {channel}:", np.sum(diff))
 
-# Wczytywanie obrazów
-img = 'man.png'
-method = 'dct'
-path = 'C:/Users/prpustel/Desktop/PROJEKT_KRYS/KRYS-Stegoanalysis/'
-original_image = cv2.imread(path + 'images/'+img)
-stego_filename = path + method+'_images/'+img.split('.')[0] + img.split('.')[1]
-stego_image = cv2.imread(stego_filename)
+image_dir = './images/'
+methods = ['lsb', 'rgba', 'dct']
 
-# Wykrywanie zmian dla kanałów RGB
-detect_stego_changes_rgb(original_image, stego_image)
+for img in os.listdir(image_dir):
+    if img.endswith(('.png', '.jpg', '.jpeg')):
+        for method in methods:
+            original_image = cv2.imread(os.path.join(image_dir, img))
+            stego_filename = os.path.join(f'./{method}_images/', img)
+            stego_image = cv2.imread(stego_filename)
+            detect_stego_changes_rgb(original_image, stego_image, method, img.split('.')[0])
